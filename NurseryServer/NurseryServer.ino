4263650cac174b471bd2c273a7cce62039009f0a
Zachary Vonler <zvonler@gmail.com>
2023-09-17 09:44:26 -0500

    Moved description to README

--- NurseryServer/NurseryServer.ino
+++ NurseryServer/NurseryServer.ino
@@ -1,40 +1,3 @@
-/**
-   Smart Nursery Server
-
-   Target: Adafruit FunHouse
-
-   The buttons on the FunHouse map to:
-     Top - increase main lights brightness
-     Middle - toggle timeout display on LED ring
-     Bottom - decrease main lights brightness
-
-   The server advertises itself at http://{hostname}.local
-   Endpoints:
-    / - General status page with buttons to perform actions
-    /brighter - Makes lights brighter
-    /dimmer - Makes lights dimmer
-    /off - Turns lights off
-    /wake - Runs a wake cycle that brings the lights up slowly
-    /status - Returns sensor and system information as JSON
-    /timeout - Toggles timeout LED ring function
-
-   Status page includes:
-     Current time
-     Last time door opened / closed
-     Last time lights on / off
-     Last time motion sensed
-     Last time noise sensed
-     Temperature
-     Humidity
-     Timeout status
-
-   The FunHouse A0 and A1 connections control the LED strips through MOSFETs.
-
-   The FunHouse A2 connection is used to power and control the LED ring.
-
-   The FunHouse I2C connection is used to talk to an MCP23008 to read the RF
-   remote receiver signals.
-*/
 
 #include "funhouse_screen.h"
 #include "led_ring.h"
@@ -69,7 +32,8 @@ const char* hostname = "nursery-devel";
 
 /*---------------------------------------------------------------------------*/
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
 
     pinMode(LED_BUILTIN, OUTPUT);
@@ -141,7 +105,8 @@ void setup() {
 
 /*---------------------------------------------------------------------------*/
 
-void loop() {
+void loop()
+{
     uint32_t now = millis();
 
     digitalWrite(LED_BUILTIN, now % 1024 < 512);
--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 
-# Nursery Server
+# Smart Nursery Server
+
+Target: [Adafruit FunHouse](https://www.adafruit.com/product/4985)
+
+The buttons on the FunHouse map to:
+ - Top - increase main lights brightness
+ - Middle - toggle timeout display on LED ring
+ - Bottom - decrease main lights brightness
+
+The server advertises itself at `http://{hostname}.local`.
+
+Endpoints:
+ - `/` - General status page with buttons to perform actions
+ - `/brighter` - Makes lights brighter
+ - `/dimmer` - Makes lights dimmer
+ - `/off` - Turns lights off
+ - `/wake` - Runs a wake cycle that brings the lights up slowly
+ - `/status` - Returns sensor and system information as JSON
+ - `/timeout` - Toggles timeout LED ring function
+
+Status page includes:
+ - Current time
+ - Last time door opened / closed
+ - Last time lights on / off
+ - Last time motion sensed
+ - Temperature
+ - Humidity
+ - Timeout status
+
+The FunHouse A0 and A1 connections control the LED strips through MOSFETs.
+
+The FunHouse A2 connection is used to power and control the LED ring.
+
+The FunHouse I2C connection is used to talk to an MCP23008 to read the RF remote receiver signals.
 
 ![Status page example](doc/status_page_example.png?raw=true "Status Page")
 
